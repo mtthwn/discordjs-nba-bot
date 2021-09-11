802f0f74bd6fec7e02ffc07b8a73435edb33b49c
Matt <myan5150@gmail.com>
2021-09-10 23:13:28 -0400

    Update linter rule
    Updating linter rule to use 2 spaces instead of tabs

--- deploy-commands.js
+++ deploy-commands.js
@@ -4,24 +4,28 @@ const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, DISCORD_TOKEN } = require('./config.json');
 
 const commands = [
-	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
-]
-	.map(command => command.toJSON());
+  new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Replies with pong!'),
+  new SlashCommandBuilder()
+    .setName('server')
+    .setDescription('Replies with server info!'),
+  new SlashCommandBuilder()
+    .setName('user')
+    .setDescription('Replies with user info!'),
+].map((command) => command.toJSON());
 
 const rest = new REST({ version: '9' }).setToken(DISCORD_TOKEN);
 
 (async () => {
-	try {
-		await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
-			{ body: commands },
-		);
+  try {
+    await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+      body: commands,
+    });
 
-		console.log('Successfully registered application commands.');
-	}
-	catch (error) {
-		console.error(error);
-	}
+    console.log('Successfully registered application commands.');
+  }
+  catch (error) {
+    console.error(error);
+  }
 })();
