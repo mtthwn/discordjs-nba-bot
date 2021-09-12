bc7a6f0ef8f5e54aa5fbac99108ff11cc1cdb440
Matt <myan5150@gmail.com>
2021-09-12 15:36:57 -0400

    Add proper stat loading for players

--- commands/nba.js
+++ commands/nba.js
@@ -8,20 +8,26 @@ const PLAYER_SEARCH_URI = 'https://balldontlie.io/api/v1/players?search=';
 const data = new SlashCommandBuilder()
   .setName('nba')
   .setDescription('Get NBA stats')
+  // .addSubcommand((subCommand) =>
+  //   subCommand
+  //     .setName('team-name')
+  //     .setDescription('Team name')
+  //     .addStringOption((option) =>
+  //       option.setName('team_name').setDescription('Team name'),
+  //     ),
+  // )
   .addSubcommand((subCommand) =>
     subCommand
-      .setName('team-name')
-      .setDescription('Team name')
+      .setName('player-search')
+      .setDescription('Player search')
       .addStringOption((option) =>
-        option.setName('team_name').setDescription('Team name'),
-      ),
-  )
-  .addSubcommand((subCommand) =>
-    subCommand
-      .setName('player-name')
-      .setDescription('Player name')
-      .addStringOption((option) =>
-        option.setName('player_name').setDescription('Player name'),
+        option
+          .setName('player_name')
+          .setDescription('Player name')
+          .setRequired(true),
+      )
+      .addIntegerOption((option) =>
+        option.setName('season').setDescription('Season').setRequired(true),
       ),
   );
 
@@ -32,28 +38,58 @@ module.exports = {
       .getString('player_name')
       .split(' ');
 
+    const season = await interaction.options.getInteger('season');
+
     const searchUrl =
       PLAYER_SEARCH_URI + `${firstName || ''} ${lastName || ''}`;
 
     const { data: playerData } = await axios.get(searchUrl);
+
+    if (!playerData) {
+      return await interaction.reply('No player found');
+    }
 
     const { data: returnedPlayers } = playerData;
 
     const embeds = [];
 
     for (const player of returnedPlayers) {
-      console.log(player.team);
+      const PLAYER_STATS_SEARCH_URI = `https://www.balldontlie.io/api/v1/season_averages?season=${season}&player_ids[]=${player.id}`;
+
+      let returnedSeasonStats;
+
+      try {
+        returnedSeasonStats = await axios.get(PLAYER_STATS_SEARCH_URI);
+      }
+      catch (e) {
+        console.error(e);
+      }
+
+      const { data: seasonData } = returnedSeasonStats.data;
+
+      if (!seasonData[0]) {
+        return await interaction.reply('Invalid season');
+      }
+
+      const { pts, games_played, min } = seasonData[0];
+
       const embed = new MessageEmbed()
         .setColor('#0099ff')
         .setTitle(`${player.first_name} ${player.last_name}`)
-        .setDescription('Player stats')
+        .setDescription(`Player stats for the ${season} season`)
         .addFields(
+          { name: 'Team', value: player.team.full_name },
           { name: 'Position', value: player.position },
           {
             name: 'Height',
             value: `${player.height_feet} ft ${player.height_inches} in`,
           },
-          { name: 'Team', value: `${player.team.full_name} ` },
+          { name: 'Games played', value: games_played.toString() },
+          { name: 'Minutes per game', value: min },
+          {
+            name: 'PPG',
+            value: pts.toString(),
+          },
         )
         .setTimestamp();
 
