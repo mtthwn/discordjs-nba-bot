9cfcf34f423f93011f92fd24175304eda5274567
mtthwn <myan5150@gmail.com>
2021-09-21 02:15:05 -0400

    Add MessageEmbed factory pattern

--- src/commands/nba-player-stats.ts
+++ src/commands/nba-player-stats.ts
@@ -1,11 +1,7 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { CommandInteraction } from 'discord.js';
 
-import Player from '../models/Player';
-import PlayerSeasonStats from '../models/PlayerSeasonStats';
-import Service from '../models/Service';
-import { PlayerSeasonAverageMessage } from '../models/MessageEmbed';
-import { InvalidSeasonError, InvalidPlayerError, NoPlayerDataFoundError } from '../models/Error';
+import MessageEmbedFactory from '../models/MessageEmbedFactory';
 
 const data = new SlashCommandBuilder()
   .setName('nba-player-stats')
@@ -43,7 +39,6 @@ const data = new SlashCommandBuilder()
 //       .setDescription('Date of the game')
 //       .setRequired(true)));
 
-const APIService = new Service();
 
 export default {
   data,
@@ -53,63 +48,7 @@ export default {
 
       await interaction.reply('Starting up the search');
 
-      let firstName: string;
-      let lastName: string;
-
-      const fullName = await interaction.options
-        .getString('player_name');
-
-      if (fullName) {
-        const splitPlayerName = fullName.split(' ');
-
-        firstName = splitPlayerName[0];
-        lastName = splitPlayerName[1];
-      }
-      else {
-        throw new InvalidPlayerError('');
-      }
-
-      const season = await interaction.options.getInteger('season');
-
-      if (!season) {
-        throw new InvalidSeasonError(null);
-      }
-
-      if (season >= 2021) {
-        throw new InvalidSeasonError('Invalid season - must be the year the season started in');
-      }
-
-      console.log(`playerData request started for ${firstName} ${lastName}`);
-
-      const { data: playerData } = await APIService.getPlayerInformation(firstName, lastName);
-
-      console.log(`playerData request complete for ${firstName} ${lastName}`);
-
-      if (!playerData.data) {
-        throw new InvalidPlayerError(`${firstName} ${lastName || ''}`);
-      }
-
-      const embeds = [];
-
-      for (const player of playerData.data) {
-        const generatedPlayer = new Player(player);
-
-        const { data: seasonData } = await APIService.getPlayerSeasonStats(season, player.id);
-
-        if (seasonData.data.length === 0) {
-          continue;
-        }
-
-        const generatedSeasonStats = new PlayerSeasonStats({ ...seasonData.data[0], season });
-
-        const embed = new PlayerSeasonAverageMessage(generatedPlayer, generatedSeasonStats);
-
-        embeds.push(embed);
-      }
-
-      if (embeds.length === 0) {
-        throw new NoPlayerDataFoundError();
-      }
+      const embeds = await MessageEmbedFactory.create('stuff', interaction);
 
       await interaction.followUp({ embeds });
     }
